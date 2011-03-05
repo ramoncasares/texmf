d64c191447b0afc37eb67739bd0b4201e3bf4808
Ramón Casares <qasar.es@gmail.com>
2011-03-05 18:58:03 +0100

    wmake.sh: mejora

--- exe/sh/wmake.sh
+++ exe/sh/wmake.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-if [ ! -d /home/papa/texmf/web2c ] ; then
- mkdir /home/papa/texmf/web2c
+if [ ! -d ~/texmf/web2c ] ; then
+ mkdir ~/texmf/web2c
 fi
-cd /home/papa/texmf/web2c
+cd ~/texmf/web2c
 tex -ini spplain \dump
 pdftex -ini spdflain \dump
 
