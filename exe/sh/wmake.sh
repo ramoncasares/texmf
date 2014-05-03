c99c5591be3ed12d6eebded2f0eeda1ce39d4207
Ramón Casares <papa@ramoncasares.com>
2014-05-03 19:57:20 +0200

    wmake.sh: tex formats created without ^^ notation for codes > 128
    Chars from 128 to 256 are considered printable.

--- exe/sh/wmake.sh
+++ exe/sh/wmake.sh
@@ -3,6 +3,6 @@ if [ ! -d ~/texmf/web2c ] ; then
  mkdir ~/texmf/web2c
 fi
 cd ~/texmf/web2c
-tex -ini spplain \dump
-pdftex -ini spdflain \dump
+tex -ini -translate-file=cp8bit.tcx spplain \dump
+pdftex -ini -translate-file=cp8bit.tcx spdflain \dump
 
